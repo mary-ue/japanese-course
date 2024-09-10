399fbb6b1ce3b03330c704326cff9a1c82e58a06
mary-ue <maryandreeva100@gmail.com>
2024-09-10 16:21:14 +0300

    childrenの渡し方

--- 04_react_basic/src/110_props_children/start/Example.jsx
+++ 04_react_basic/src/110_props_children/start/Example.jsx
@@ -1,15 +1,24 @@
-import Profile from "./components/Profile";
-import Container from "./components/Container";
+import Profile from './components/Profile';
+import Container from './components/Container';
 
 const profile = [
-  { name: "Takashi", age: 19, country: "Japan", color: "green" },
-  { name: "Jane", age: 28, country: "UK", color: "blue" },
+  { name: 'Takashi', age: 19, country: 'Japan', color: 'green' },
+  { name: 'Jane', age: 28, country: 'UK', color: 'blue' },
 ];
 
 const Example = () => {
   return (
     <div>
-      <Container title="Childrenとは？" />
+      <Container title="順番通り渡す">
+        <Profile key={profile[0].name} {...profile[0]} />
+        <Profile key={profile[1].name} {...profile[1]} color="red" />
+      </Container>
+
+      <Container
+        title="個別渡す"
+        first={<Profile key={profile[0].name} {...profile[1]} color="red" />}
+        second={<Profile key={profile[1].name} {...profile[0]} />}
+      />
     </div>
   );
 };
