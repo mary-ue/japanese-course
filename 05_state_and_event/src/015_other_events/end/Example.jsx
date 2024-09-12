7645e01bc0cf71a9e73c695dbb3abb9e5c8d8bca
mary-ue <maryandreeva100@gmail.com>
2024-09-13 00:35:13 +0300

    イベントタイプ

--- 05_state_and_event/src/015_other_events/end/Example.jsx
+++ 05_state_and_event/src/015_other_events/end/Example.jsx
@@ -1,4 +1,4 @@
-import "./Example.css";
+import './Example.css';
 
 /* POINT イベントリスナの登録方法 */
 const Example = () => {
@@ -9,23 +9,22 @@ const Example = () => {
         入力値のイベント：
         <input
           type="text"
-          onChange={() => console.log("onChange検知")}
-          onBlur={() => console.log("onBlur検知")}
-          onFocus={() => console.log("onFocus検知")}
+          // onChangeはJSと違って、入力値が変更されたタイミングで発火する
+          onChange={() => console.log('onChange検知')}
+          onBlur={() => console.log('onBlur検知')}
+          onFocus={() => console.log('onFocus検知')}
         />
       </label>
-      {/* POINT e.target.valueで入力値を取得 */}
       <div>
         <label>
           入力値を取得：
           <input type="text" onChange={(e) => console.log(e.target.value)} />
         </label>
       </div>
-      {/* POINT 複数のイベントを登録 */}
       <div
         className="hover-event"
-        onMouseEnter={() => console.log("カーソルが入ってきました。")}
-        onMouseLeave={() => console.log("カーソルが出ていきました。")}
+        onMouseEnter={() => console.log('カーソルが入ってきました。')}
+        onMouseLeave={() => console.log('カーソルが出ていきました。')}
       >
         ホバーしてね！
       </div>
